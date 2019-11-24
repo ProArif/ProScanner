4b653abe08b3935cdd09c4c4aa8062b403724f13
Md Ariful Islam <34057291+ProArif@users.noreply.github.com>
2019-11-24 11:25:29 +0600

    Initial commit with completed gradle dependencies

---
+++ app/src/main/java/com/arif/nodeers/proscanner/MainActivity.java
@@ -0,0 +1,47 @@
+package com.arif.nodeers.proscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.otaliastudios.cameraview.CameraView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraView cameraView;
+    private boolean isDetected = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                        Toast.makeText(MainActivity.this,"Camera Permission is mandatory",Toast.LENGTH_LONG).show();
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+}
